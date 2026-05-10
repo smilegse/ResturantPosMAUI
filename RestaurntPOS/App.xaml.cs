2d7779b9ed991a87f7aeb58a70a6865f98404452
smilegse <abms.siddique@gmail.com>
2026-05-10 23:50:01 +0600

    first commit

---
+++ RestaurntPOS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurntPOS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RestaurntPOS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurntPOS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
